9cd22f889e059bbe30277b823b8140d5b1fafa72
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-07-15 14:47:33 +0700

    Add files via upload

--- js/getdata.js
+++ js/getdata.js
@@ -20,6 +20,58 @@ var db = firebase.firestore().collection("CheckProfile");
 //document.getElementById('CheckProfile').style.display='none';
 //document.getElementById('CheckConfirm').style.display='none';
 
+
+
+
+async function main() {
+  await liff.init({ liffId: "1655966947-5rJYErwX" })
+  document.getElementById("isLoggedIn").append(liff.isLoggedIn())
+  if(liff.isLoggedIn()) {
+    getUserProfile()
+  } else {
+    liff.login()
+  }
+}
+main()
+
+async function getUserProfile() {
+	const profile = await liff.getProfile()
+	document.getElementById("pictureUrl").src = profile.pictureUrl
+	document.getElementById("userId").append(profile.userId)
+	document.getElementById("displayName").append(profile.displayName)
+	document.getElementById("statusMessage").append(profile.statusMessage)
+	document.getElementById("decodedIDToken").append(liff.getDecodedIDToken().email)
+	vLineID = append(profile.userId);
+	vLineName = append(profile.displayName);
+	vLinePicture = profile.pictureUrl;
+
+}
+
+alert(vLineID);
+
+function logOut() {
+  liff.logout()
+  window.location.reload()
+}
+
+function closed() {
+  liff.closeWindow()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 async function main() {
   //alert("LINE Developers x Skooldio")
   await liff.init({ liffId: "1655966947-5rJYErwX" })
