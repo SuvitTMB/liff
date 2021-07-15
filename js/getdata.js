35713ce72a143f35915b1fc001a084d1a19b7699
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-07-15 16:12:51 +0700

    Add files via upload

--- js/getdata.js
+++ js/getdata.js
@@ -2,6 +2,56 @@ var vLineID = "";
 var vLineName = "";
 var vLinePicture = "";
 
+
+
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDfTJJ425U4OY0xac6jdhtSxDeuJ-OF-lE",
+    authDomain: "retailproject-6f4fc.firebaseapp.com",
+    projectId: "retailproject-6f4fc",
+    storageBucket: "retailproject-6f4fc.appspot.com",
+    messagingSenderId: "653667385625",
+    appId: "1:653667385625:web:a5aed08500de80839f0588",
+    measurementId: "G-9SKTRHHSW9"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  //firebase.analytics();
+  var db = firebase.firestore().collection("CheckProfile");
+  document.getElementById('CheckLogin').style.display='block';
+  document.getElementById('CheckProfile').style.display='none';
+  document.getElementById('CheckConfirm').style.display='none';
+
+
+    async function main() {
+      await liff.init({ liffId: "1655966947-5rJYErwX" });
+      document.getElementById("isLoggedIn").append(liff.isLoggedIn());
+      if(liff.isLoggedIn()) {
+        getUserProfile();
+      } else {
+        liff.login();
+      }
+    }
+    main()
+
+
+    async function getUserProfile() {
+      const profile = await liff.getProfile();
+      document.getElementById("pictureUrl").src = profile.pictureUrl;
+      document.getElementById("pictureUrl1").src = profile.pictureUrl;
+      document.getElementById("userId").append(profile.userId);
+      document.getElementById("displayName").append(profile.displayName);
+      document.getElementById("displayName1").append(profile.displayName);
+      aPicture = profile.pictureUrl;
+      alineID = profile.userId;
+      aLinename = profile.displayName;
+      alert(aPicture);
+    }
+
+
+
+
+/*
   var firebaseConfig = {
     apiKey: "AIzaSyDfTJJ425U4OY0xac6jdhtSxDeuJ-OF-lE",
     authDomain: "retailproject-6f4fc.firebaseapp.com",
