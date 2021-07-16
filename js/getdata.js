9fc292a02385fdc7f067ab78be0fc65f8c984508
SuvitTMB <82767838+SuvitTMB@users.noreply.github.com>
2021-07-16 14:36:33 +0700

    Add files via upload

--- js/getdata.js
+++ js/getdata.js
@@ -3,7 +3,7 @@ var vLineName = "";
 var vLinePicture = "";
 
 
-/*
+
 var firebaseConfig = {
 	apiKey: "AIzaSyDfTJJ425U4OY0xac6jdhtSxDeuJ-OF-lE",
 	authDomain: "retailproject-6f4fc.firebaseapp.com",
@@ -14,46 +14,73 @@ var firebaseConfig = {
 	measurementId: "G-9SKTRHHSW9"
 };
 firebase.initializeApp(firebaseConfig);
-//firebase.analytics();
-//var db=firebase.firestore().collection("personal")
-*/
+var db = firebase.firestore().collection("CheckProfile");
 
-    async function main() {
-      await liff.init({ liffId: "1655966947-5rJYErwX" });
-      document.getElementById("isLoggedIn").append(liff.isLoggedIn());
-      if(liff.isLoggedIn()) {
-        getUserProfile();
-      } else {
-        liff.login();
-      }
+
+async function main() {
+	await liff.init({ liffId: "1655966947-5rJYErwX" });
+	document.getElementById("isLoggedIn").append(liff.isLoggedIn());
+	if(liff.isLoggedIn()) {
+	getUserProfile();
+	} else {
+	liff.login();
+	}
+}
+main();
+
+
+function openWindow() {
+	liff.openWindow({
+	url: "https://line.me",
+	external: true     
+	})
+}
+
+
+async function getUserProfile() {
+	const profile = await liff.getProfile();
+	//document.getElementById("DisplayLineID").src = profile.pictureUrl;
+	//document.getElementById("displayName").append(profile.displayName);
+	vLineID = profile.userId;
+	vLineName = profile.displayName;
+	vLinePicture = profile.pictureUrl;
+	document.getElementById("pictureUrl1").src = profile.pictureUrl;
+	//alert(vLinePicture);
+	//alert(profile.userId+"---"+profile.displayName+"---"+profile.pictureUrl);
+	//var ShowImg = '<div><img src="'+profile.pictureUrl+'" style="width:130px;"></div>';
+	//document.getElementById("DisplayURL").src = profile.pictureUrl;
+	document.getElementById("DisplayLineID").append(profile.userId);
+	document.getElementById("DisplayName").append(profile.displayName);
+	sessionStorage.setItem("LineID", profile.userId);
+	sessionStorage.setItem("LineName", profile.displayName);
+	sessionStorage.setItem("LinePicture", profile.pictureUrl);
+	CheckLineID(profile.userId);
+  //sessionStorage.setItem("LineID", profile.userId);
+}
+
+var CheckUserID = 0;
+function ReadData(gLineID) {
+	db.where('lineID','==',gLineID).get().then((snapshot)=> {
+		snapshot.forEach(doc=> {
+		vLineID = doc.data().linename;
+		CheckUserID = 1;
+	});
+	SaveProfile();
+}
+
+function SaveProfile() {
+ 	var dateString = new Date().toLocaleString('en-US', { timeZone: 'Asia/Jakarta' });
+    if(aCheck==1) {
+		db.doc(Eid).update({
+        lastcheckin : dateString,
+      });
+    } else {
+      db.add({
+        lineID : vLineID,
+        linename : vLineName,
+        empPicture : vLinePicture,
+        lastcheckin : dateString,
+      });       
     }
-    main();
 
-
-    async function getUserProfile() {
-      const profile = await liff.getProfile();
-      //document.getElementById("DisplayLineID").src = profile.pictureUrl;
-      //document.getElementById("displayName").append(profile.displayName);
-      vLinePicture = profile.pictureUrl;
-      vLineID = profile.userId;
-      vLineName = profile.displayName;
-      document.getElementById("pictureUrl1").src = profile.pictureUrl;
-      //alert(vLinePicture);
-      //alert(profile.userId+"---"+profile.displayName+"---"+profile.pictureUrl);
-      //var ShowImg = '<div><img src="'+profile.pictureUrl+'" style="width:130px;"></div>';
-      //document.getElementById("DisplayURL").src = profile.pictureUrl;
-      document.getElementById("DisplayLineID").append(profile.userId);
-      document.getElementById("DisplayName").append(profile.displayName);
-      sessionStorage.setItem("LineID", profile.userId);
-      sessionStorage.setItem("LineName", profile.displayName);
-      sessionStorage.setItem("LinePicture", profile.pictureUrl);
-      //sessionStorage.setItem("LineID", profile.userId);
-    }
-
-
-    function openWindow() {
-      liff.openWindow({
-        url: "https://line.me",
-        external: true     
-      })
-    }
+}
